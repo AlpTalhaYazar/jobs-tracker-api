07e27dc47d0ecfffdab5a284e5204aec880ab03c
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2024-12-08 01:02:08 +0300

    refactor: migrate to ES modules and update error handling middleware

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-require('dotenv').config();
-require('express-async-errors');
-const express = require('express');
+import "dotenv/config";
+import "express-async-errors";
+import express from "express";
+import { notFoundMiddleware } from "./middleware/not-found.js";
+import { errorHandlerMiddleware } from "./middleware/error-handler.js";
+
 const app = express();
 
-// error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
-
 app.use(express.json());
-// extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
+app.get("/", (req, res) => {
+  res.send("jobs api");
 });
 
 app.use(notFoundMiddleware);
