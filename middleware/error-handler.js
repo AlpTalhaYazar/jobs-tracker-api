3da4c1764f23a65b2229be2d60466c3726de8515
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2024-12-11 01:21:29 +0300

    feat: update error handler middleware to use Result for structured error responses

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,11 +1,12 @@
 import { StatusCodes } from "http-status-codes";
-import { CustomAPIError } from "../errors/custom-api.js";
+import { Result } from "../utils/Result.js";
 
-const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
-  }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+const errorHandlerMiddleware = async (err, req, res, next) => {
+  const result = Result.failure(err.message);
+
+  return res
+    .status(err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR)
+    .json(result);
 };
 
 export { errorHandlerMiddleware };
