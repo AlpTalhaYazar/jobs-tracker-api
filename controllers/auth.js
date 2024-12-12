c04468b04c4245c2ef382b3c12fdca5e34cdd361
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2024-12-12 20:37:21 +0300

    feat: implement register controller with JWT token generation and structured response

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,24 @@ import { StatusCodes } from "http-status-codes";
 import { BadRequestError } from "../errors/index.js";
 import { Result } from "../utils/Result.js";
 
+const register = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    throw BadRequestError("Please provide username and password");
+  }
+
+  const id = new Date().getDate();
+
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  const result = Result.success({ id, username, token });
+
+  res.status(StatusCodes.CREATED).json(result);
+}
+
 const login = async (req, res) => {
   const { username, password } = req.body;
 
@@ -21,4 +39,4 @@ const login = async (req, res) => {
   res.status(StatusCodes.OK).json(result);
 };
 
-export { login };
+export { register, login };
