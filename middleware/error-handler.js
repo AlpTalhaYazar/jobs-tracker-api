58b2152e4e7bc9b59285508dadcf6dd6ae1f0125
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2024-12-16 20:55:35 +0300

    feat: enhance error handler to include validation error details

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -2,7 +2,13 @@ import { StatusCodes } from "http-status-codes";
 import { Result } from "../utils/Result.js";
 
 const errorHandlerMiddleware = async (err, req, res, next) => {
-  const result = Result.failure(err.message);
+  const error = { message: err.message, details: err.errors };
+  if (err.name === "ValidationError") {
+    const result = Result.failure(error);
+    return res.status(err.statusCode).json(result);
+  }
+
+  const result = Result.failure(error);
 
   return res
     .status(err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR)
