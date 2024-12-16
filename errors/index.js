aaba03a2f5e75b9f255d8b22aef722b0a14c79fd
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2024-12-16 20:53:14 +0300

    feat: export ValidationError class for improved error handling

--- errors/index.js
+++ errors/index.js
@@ -2,5 +2,6 @@ import { CustomError, createCustomError } from "./custom-error.js";
 import { UnauthenticatedError } from "./unauthenticated.js";
 import { NotFoundError } from "./not-found.js";
 import { BadRequestError } from "./bad-request.js";
+import { ValidationError } from "./validation-error.js";
 
-export { CustomError, createCustomError, UnauthenticatedError, NotFoundError, BadRequestError };
+export { CustomError, createCustomError, UnauthenticatedError, NotFoundError, BadRequestError, ValidationError };
