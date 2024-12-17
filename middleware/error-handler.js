637e2f087a28ed85f06de51b06165254640646e4
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2024-12-17 13:44:02 +0300

    feat: improve error handler to use default status code for validation errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -4,8 +4,9 @@ import { Result } from "../utils/Result.js";
 const errorHandlerMiddleware = async (err, req, res, next) => {
   const error = { message: err.message, details: err.errors };
   if (err.name === "ValidationError") {
+    const statusCode = err.statusCode || StatusCodes.BAD_REQUEST;
     const result = Result.failure(error);
-    return res.status(err.statusCode).json(result);
+    return res.status(statusCode).json(result);
   }
 
   const result = Result.failure(error);
