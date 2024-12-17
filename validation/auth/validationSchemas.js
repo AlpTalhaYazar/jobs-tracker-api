02035131c740d99a439e2502c4656118486ba041
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2024-12-17 12:56:00 +0300

    feat: add trimming to validation schemas for registration and login

--- validation/auth/validationSchemas.js
+++ validation/auth/validationSchemas.js
@@ -4,17 +4,25 @@ export const registerSchema = [
   body("name")
     .isString()
     .withMessage("Name must be a string")
+    .trim()
     .isLength({ min: 3, max: 50 })
     .withMessage("Name must be between 3 and 50 characters"),
-  body("email").isEmail().withMessage("Please provide a valid email address"),
+  body("email")
+    .trim()
+    .isEmail()
+    .withMessage("Please provide a valid email address"),
   body("password")
     .isString()
     .withMessage("Password must be a string")
+    .trim()
     .isLength({ min: 6 })
     .withMessage("Password must be at least 6 characters"),
 ];
 
 export const loginSchema = [
-  body("email").isEmail().withMessage("Please provide a valid email address"),
-  body("password").isString().withMessage("Password must be a string"),
+  body("email")
+    .trim()
+    .isEmail()
+    .withMessage("Please provide a valid email address"),
+  body("password").isString().withMessage("Password must be a string").trim(),
 ];
