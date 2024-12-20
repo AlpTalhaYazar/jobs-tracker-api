58fb985dda65a86cc66fc49b25d6359a94a08127
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2024-12-20 13:48:52 +0300

    feat: refactor user registration to include user model validation and JWT token generation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,27 +1,43 @@
+import "dotenv/config";
 import jwt from "jsonwebtoken";
-import { RegisterDto, LoginDto } from "../dto/auth.dto.js";
+import bcrypt from "bcryptjs";
+import { User } from "../models/User.js";
 import { Result } from "../utils/Result.js";
 import { StatusCodes } from "http-status-codes";
 import { BadRequestError } from "../errors/index.js";
+import { RegisterDto, LoginDto } from "../dto/auth.dto.js";
 
 const register = async (req, res) => {
-  const registerModel = new RegisterDto(
+  const newUserDto = new RegisterDto(
     req.body.name,
     req.body.email,
     req.body.password
   );
 
-  if (!name || !password) {
-    throw BadRequestError("Please provide name and password");
-  }
-
-  const id = new Date().getDate();
-
-  const token = jwt.sign({ id, name }, process.env.JWT_SECRET, {
-    expiresIn: "1d",
+  const user = new User({
+    name: newUserDto.name,
+    email: newUserDto.email,
+    password: newUserDto.password,
   });
 
-  const result = Result.success({ id, name, token });
+  await user.validate();
+
+  await user.save();
+
+  const token = jwt.sign(
+    { id: user._id, name: user.name, email: user.email },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1d",
+    }
+  );
+
+  const result = Result.success({
+    id: user._id,
+    name: user.name,
+    email: user.email,
+    token,
+  });
 
   res.status(StatusCodes.CREATED).json(result);
 };
