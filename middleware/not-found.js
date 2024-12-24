72fdae358d73192e12c0a794651485c6b8915cf2
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2024-12-24 21:08:29 +0300

    feat: enhance not found middleware to use standardized operation and API response handling

--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,9 +1,18 @@
 import { StatusCodes } from "http-status-codes";
-import { Result } from "../utils/Result.js";
+import { OperationResult } from "../utils/OperationResult.js";
+import { ErrorCode } from "../utils/ErrorCode.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
-const notFoundMiddleware = (req, res) =>
-  res
-    .status(StatusCodes.NOT_FOUND)
-    .json(Result.failure("Route does not exist"));
+const notFoundMiddleware = async (req, res) => {
+  const operationResult = await OperationResult.Failure(
+    ErrorCode.NOT_FOUND,
+    "Route does not exist",
+    ["Route does not exist"]
+  );
+
+  const apiResponse = await ApiResponse.ToApiResponse(operationResult);
+
+  return res.status(StatusCodes.NOT_FOUND).json(apiResponse);
+};
 
 export { notFoundMiddleware };
