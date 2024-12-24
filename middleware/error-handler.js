661bf48f8f2ca15aa7a549f440acda0bc57b1e97
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2024-12-24 22:08:55 +0300

    feat: enhance error handler middleware for standardized error response handling

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,19 +1,35 @@
 import { StatusCodes } from "http-status-codes";
-import { Result } from "../utils/Result.js";
+import { ErrorCode } from "../utils/ErrorCode.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { OperationResult } from "../utils/OperationResult.js";
 
 const errorHandlerMiddleware = async (err, req, res, next) => {
   const error = { message: err.message, details: err.errors };
   if (err.name === "ValidationError") {
     const statusCode = err.statusCode || StatusCodes.BAD_REQUEST;
-    const result = Result.failure(error);
-    return res.status(statusCode).json(result);
+
+    const operationResult = await OperationResult.Failure(
+      ErrorCode.ValidationError,
+      err.message,
+      err.errors
+    );
+
+    const apiResponse = await ApiResponse.ToApiResponse(operationResult);
+
+    return res.status(statusCode).json(apiResponse);
   }
 
-  const result = Result.failure(error);
+  const operationResult = await OperationResult.Failure(
+    ErrorCode.InternalServerError,
+    err.message,
+    err.errors
+  );
+
+  const apiResponse = await ApiResponse.ToApiResponse(operationResult);
 
   return res
     .status(err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR)
-    .json(result);
+    .json(apiResponse);
 };
 
 export { errorHandlerMiddleware };
